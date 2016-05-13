e399f33731bbd97020a7d84cc7948273d513ba95
Rahul Kesavan <b_k_rahul@yahoo.com>
2016-05-13 07:47:32 -0700

    Multiple Components
    Created a reusable component
    Make a component accept input
    Bind a parent component to a child component
    Declare application directives in directives array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+
 @Component({
     selector: 'my-app',
     template: `
@@ -10,16 +13,10 @@ import {Component} from '@angular/core';
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-    <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-        </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
-    styleUrls: ['../styles.css']
+    styleUrls: ['../styles.css'],
+    directives: [HeroDetailComponent] 
 })
 export class AppComponent {
     title: string = 'Tour of Heroes';  
@@ -29,11 +26,6 @@ export class AppComponent {
     onSelect(hero:Hero) {
         this.selectedHero = hero;
     }
-}
-
-export class Hero {
-    id: number;
-    name: string;
 }
 
 var HEROES: Hero[] = [
